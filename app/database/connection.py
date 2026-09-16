bfaea1f99e34d2e25960fc8d5c07a6e4df21f756
jeferson-estiven-vargas <estivegarcia51@gmail.com>
2026-09-15 22:49:17 -0500

    Configurar conexion a la base de datos con SQLAlchemy

---
+++ app/database/connection.py
@@ -0,0 +1,11 @@
+# app/database/connection.py
+# Configuracion de la base de datos: engine, sesion y base declarativa
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "sqlite:///./device_systems.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
